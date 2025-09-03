9f2616a051b669f3baf0c07fcbc59bfd42a36696
Aksh Khurana <khuranaaksh08@gmail.com>
2025-09-04 00:21:29 +0530

    supabase basic part completed

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,14 @@ import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient, processLock } from '@supabase/supabase-js'
 
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const anon = process.env.EXPO_PUBLIC_SUPABASE_KEY;
+
+if (!supabaseUrl) throw new Error('supabaseUrl is required.');
+if (!anon) throw new Error('supabase anon key is required.');
+
 export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+  supabaseUrl,anon,
   {
     auth: {
       storage: AsyncStorage,
